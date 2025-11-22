189f611a9b63632d7a1c2aef1729637aaa8ad51c
kirill <khodarevskii@yandex.ru>
2025-11-22 10:33:21 +0300

    задача 17

--- src/components/app-header/app-header.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react';
-import { AppHeaderUI } from '@ui';
-import { useSelector } from '../../services/store';
-
-export const AppHeader: FC = () => {
-  const user = useSelector((state) => state.user.user);
-  return <AppHeaderUI userName={user?.name || ''} />;
-};
